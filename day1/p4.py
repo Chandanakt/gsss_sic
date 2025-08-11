d939fd5fe03b09f273a2bac406f22609d4b0a01f
Chandanakt <chandanakt1712@gmail.com>
2025-08-11 16:26:38 +0530

    this contains program about print function and even or not

---
+++ day1/p4.py
@@ -0,0 +1,6 @@
+input_number=int(input('Enter a number to check if it is Even: '))
+
+if input_number % 2 == 0:
+    print("The given number ',input_number,' is an Even number")
+else:
+    print("The given number ',input_number,' is not an Even number")
