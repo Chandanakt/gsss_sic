89daf9b59e87f147769df94a819b81213f283a9e
Chandanakt <chandanakt1712@gmail.com>
2025-08-18 09:28:56 +0530

    day 3

--- day2/student_result.py
+++ day2/student_result.py
@@ -1,4 +1,8 @@
-...
-#accept the average score
-...
+
+# Accept the average score of the student and print the result as follows:
+# 0  to 59     Fail
+# 60 to 84     Second class
+# 85 to 95     First class
+# 96 to 100    Excellent
+# Also check for invalid score. No negative marking.
 
