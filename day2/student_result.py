036c105636c313135e979e9a937e304a5ade7c9e
Chandanakt <chandanakt1712@gmail.com>
2025-09-01 10:30:43 +0530

    day 2 notes and programs

--- day2/student_result.py
+++ day2/student_result.py
@@ -6,3 +6,14 @@
 # 96 to 100    Excellent
 # Also check for invalid score. No negative marking.
 
+average_score = int(input('Enter your average score to print the result: '))
+if average_score >= 0 and average_score <= 59:
+    print('Result is Fail')
+elif average_score <= 84:
+    print('Result is Second class')
+elif average_score <= 95:
+    print('Result is First class')
+elif average_score <= 100:
+    print('Result is Excellent')
+else:
+    print('Invalid Score')
